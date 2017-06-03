3113f67e0d297eab81b84b11dd605094ac5bb53d
Cong Min <i@congm.in>
2017-06-03 16:11:27 +0800

    init

---
+++ src/components/sideBarPreset/weibo.js
@@ -0,0 +1,11 @@
+// 微博
+export default [
+  {
+    name: 'visual',
+    title: '微博',
+    icon: 'ios-pulse-strong',
+    children: [
+      { name: 'visual', title: '综合统计与可视化分析', pageType: 'visual' },
+    ],
+  },
+];
---
+++ src/components/sideBarPreset/zhihu.js
@@ -0,0 +1,11 @@
+// 知乎
+export default [
+  {
+    name: 'visual',
+    title: '知乎',
+    icon: 'ios-pulse-strong',
+    children: [
+      { name: 'visual', title: '综合统计与可视化分析', pageType: 'visual' },
+    ],
+  },
+];
